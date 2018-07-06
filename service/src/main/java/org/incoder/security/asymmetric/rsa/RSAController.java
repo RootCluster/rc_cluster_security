86ebfceac82be06c535cccf1b6afd4365e4fa9e0
Jerry xu <Jerry.x@outlook.com>
2018-07-06 21:59:42 +0800

    init project

--- service/src/main/java/org/incoder/security/asymmetric/rsa/RSAController.java
+++ service/src/main/java/org/incoder/security/asymmetric/rsa/RSAController.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import sun.misc.BASE64Decoder;
 
 import javax.crypto.Cipher;
+import java.io.*;
 import java.security.*;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Jerry xu
@@ -19,16 +25,33 @@ import java.util.Map;
 public class RSAController {
 
     /**
+     * 指定加密算法为RSA
+     */
+    private static final String KEY_ALGORITHM = "RSA";
+    /**
+     * 密钥长度，用来初始化
+     */
+    private static int KEY_SIZE = 1024;
+    /**
+     * 指定公钥存放文件
+     */
+    private static String PUBLIC_KEY_FILE = "PublicKey";
+    /**
+     * 指定私钥存放文件
+     */
+    private static String PRIVATE_KEY_FILE = "PrivateKey";
+
+    /**
      * 获取RSA公钥
      *
      * @return getPublicKey
-     * @throws NoSuchAlgorithmException 算法异常
+     * @throws IOException 算法异常
      */
     @ResponseBody
     @RequestMapping(value = "getPublicKey", method = RequestMethod.GET)
-    public Object getPublicKey() throws NoSuchAlgorithmException {
-        Map<String, byte[]> result = new HashMap<>();
-        result.put("publicKey", Base64.getEncoder().encode(buildKeyPair(1024).getPublic().getEncoded()));
+    public Object getPublicKey() throws IOException {
+        Map<String, String> result = new HashMap<>();
+        result.put("publicKey", getKeyPairString(PUBLIC_KEY_FILE));
         return result;
     }
 
@@ -36,13 +59,13 @@ public class RSAController {
      * 获取RSA私钥
      *
      * @return getPrivateKey
-     * @throws NoSuchAlgorithmException 算法异常
+     * @throws IOException 算法异常
      */
     @ResponseBody
     @RequestMapping(value = "getPrivateKey", method = RequestMethod.GET)
-    public Object getPrivateKey() throws NoSuchAlgorithmException {
-        Map<String, byte[]> result = new HashMap<>();
-        result.put("getPrivateKey", Base64.getEncoder().encode(buildKeyPair(1024).getPrivate().getEncoded()));
+    public Object getPrivateKey() throws IOException {
+        Map<String, String> result = new HashMap<>();
+        result.put("getPrivateKey", getKeyPairString(PRIVATE_KEY_FILE));
         return result;
     }
 
@@ -56,7 +79,10 @@ public class RSAController {
     public boolean decryptClient(MessageBean message) throws Exception {
         if (null != message) {
             // 解密客户端公钥加密内容
-            String serverDecryptContent = new String(decrypt(buildKeyPair(1024).getPrivate(), message.getEncryptContent().getBytes()));
+            // 转换私钥
+            PrivateKey privateKey = getPrivateKeyFromString(getKeyPairString(PRIVATE_KEY_FILE));
+
+            String serverDecryptContent = new String(decrypt(privateKey, message.getEncryptContent().getBytes()));
             System.out.println("客户端原数据（未公钥加密）：" + message.getUnencryptedContent());
             System.out.println("服务端解密客户端公钥数据：" + serverDecryptContent);
             return serverDecryptContent.equals(message.getUnencryptedContent());
@@ -67,13 +93,87 @@ public class RSAController {
     /**
      * 生成RSA密钥对
      *
-     * @return 公钥
-     * @throws NoSuchAlgorithmException 异常
+     * @throws Exception 异常
      */
-    public static KeyPair buildKeyPair(int keySize) throws NoSuchAlgorithmException {
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        keyPairGenerator.initialize(keySize);
-        return keyPairGenerator.generateKeyPair();
+    public static void generateKeyPair() throws Exception {
+        // 为RSA算法创建一个KeyPairGenerator对象
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+        // 利用上面的随机数据源初始化这个KeyPairGenerator对象
+        keyPairGenerator.initialize(KEY_SIZE, new SecureRandom());
+        // 生成密匙对
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        // 得到公钥
+        Key publicKey = keyPair.getPublic();
+        // 得到私钥
+        Key privateKey = keyPair.getPrivate();
+        // 得到公钥字符串
+        String publicKeyString = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+        // 得到私钥字符串
+        String privateKeyString = Base64.getEncoder().encodeToString(privateKey.getEncoded());
+
+        OutputStreamWriter oos1 = null;
+        OutputStreamWriter oos2 = null;
+
+        try {
+            // 用对象流将生成的密钥写入文件
+            oos1 = new OutputStreamWriter(new FileOutputStream(PUBLIC_KEY_FILE));
+            oos2 = new OutputStreamWriter(new FileOutputStream(PRIVATE_KEY_FILE));
+            oos1.write(publicKeyString, 0, publicKeyString.length());
+            oos2.write(privateKeyString, 0, privateKeyString.length());
+        } finally {
+            // 清空缓存，关闭文件输出流
+            Objects.requireNonNull(oos1).close();
+            Objects.requireNonNull(oos2).close();
+        }
+    }
+
+    /**
+     * 根据文件获取密钥字符串
+     *
+     * @param keyFile 文件
+     * @return 密钥字符串
+     * @throws IOException IO异常
+     */
+    private String getKeyPairString(String keyFile) throws IOException {
+        if (keyFile != null) {
+            InputStreamReader isr = new InputStreamReader(new FileInputStream(keyFile));
+            StringBuilder clientPrivateKey = new StringBuilder();
+            int i;
+            while ((i = isr.read()) != -1) {
+                clientPrivateKey.append((char) i);
+            }
+            isr.close();
+            return clientPrivateKey.toString();
+        }
+        return null;
+    }
+
+    /**
+     * 公钥字符串转PublicKey
+     *
+     * @param publicKeyString 公钥字符串
+     * @return PublicKey
+     * @throws Exception 异常
+     */
+    public static PublicKey getPublicKeyFromString(String publicKeyString) throws Exception {
+        BASE64Decoder b64 = new BASE64Decoder();
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(b64.decodeBuffer(publicKeyString));
+        return keyFactory.generatePublic(publicKeySpec);
+    }
+
+    /**
+     * 私钥字符串转PrivateKey
+     *
+     * @param privateKeyString 私钥字符串
+     * @return PrivateKey
+     * @throws Exception 异常
+     */
+    private static PrivateKey getPrivateKeyFromString(String privateKeyString) throws Exception {
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        BASE64Decoder b64 = new BASE64Decoder();
+        EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(b64.decodeBuffer(privateKeyString));
+        return keyFactory.generatePrivate(privateKeySpec);
     }
 
     /**
@@ -84,8 +184,8 @@ public class RSAController {
      * @return byte
      * @throws Exception 异常
      */
-    public static byte[] encrypt(PublicKey publicKey, byte[] encrypted) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
+    private static byte[] encrypt(PublicKey publicKey, byte[] encrypted) throws Exception {
+        Cipher cipher = Cipher.getInstance(KEY_ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
         return cipher.doFinal(encrypted);
     }
@@ -98,8 +198,8 @@ public class RSAController {
      * @return byte
      * @throws Exception 异常
      */
-    public static byte[] decrypt(PrivateKey privateKey, byte[] message) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
+    private static byte[] decrypt(PrivateKey privateKey, byte[] message) throws Exception {
+        Cipher cipher = Cipher.getInstance(KEY_ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
         return cipher.doFinal(message);
     }
--- service/src/test/java/org/incoder/security/RSATest.java
+++ service/src/test/java/org/incoder/security/RSATest.java
@@ -1,10 +1,7 @@
 package org.incoder.security;
 
-import java.security.*;
 
-import static org.incoder.security.asymmetric.rsa.RSAController.buildKeyPair;
-import static org.incoder.security.asymmetric.rsa.RSAController.decrypt;
-import static org.incoder.security.asymmetric.rsa.RSAController.encrypt;
+import static org.incoder.security.asymmetric.rsa.RSAController.*;
 
 /**
  * @author Jerry xu
@@ -13,21 +10,9 @@ import static org.incoder.security.asymmetric.rsa.RSAController.encrypt;
 public class RSATest {
 
     public static void main(String[] args) throws Exception {
+        // 生成密钥对
+        generateKeyPair();
 
-        // generate public and private keys
-        KeyPair keyPair = buildKeyPair(2048);
-        PublicKey pubKey = keyPair.getPublic();
-        PrivateKey privateKey = keyPair.getPrivate();
-        String message = "This is a message";
-
-        // encrypt the message
-        byte[] encrypted = encrypt(pubKey, message.getBytes());
-        System.out.println("加密结果：\n" + new String(encrypted));
-
-        // decrypt the message
-        byte[] secret = decrypt(privateKey, encrypted);
-        System.out.println("解密结果：" + new String(secret));
     }
-
 
 }
